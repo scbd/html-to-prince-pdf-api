21bd16ce192b56f75614077085f1a5f5bdcec39b
Blaise Fonseca <blaisefonseca@hotmail.com>
2018-10-28 19:14:02 -0400

    async error handling

--- src/router.js
+++ src/router.js
@@ -2,11 +2,13 @@ const _ = require('lodash');
 const express = require('express');
 const renderer = require('./renderer');
 
+function asyncwrap(fn) { return function (req, res, next) { fn(req, res, next).catch(next); } }
+
 function createRouter() {
 
   const router = express.Router();
 
-  router.post ('/api/render',        validate,  renderer.renderPdf);
+  router.post ('/api/render',        validate, asyncwrap(renderer.renderPdf));
 
   return router;
 
