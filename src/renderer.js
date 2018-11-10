137ebb9e633521a8789c971843ba85e166a6dd28
Blaise Fonseca <blaisefonseca@hotmail.com>
2018-11-10 12:40:26 +0200

    add try catch

--- src/renderer.js
+++ src/renderer.js
@@ -17,37 +17,44 @@ const Whitelist_PDFParams = [
 
 async function renderPdf(req, res) {
 
-    let pdfOptions = {};
-    
-    let options = _.defaults({
-          media     : 'screen',
-          'page-margin': '0.5cm',
-      }, req.query);
-      
-    if(options){
-        _.each(Whitelist_PDFParams, function(param){
-            if(options[param])
-                pdfOptions[param] = options[param];
-        });
-    }
-    
-    let pageContent = req.body;
-    
-    winston.info(`Render Prince PDF for HTML length ${pageContent.length}`);
-
-    if(config.PRINCE_PDF_LICENSE_FILE)
-        pdfOptions['license-file'] = config.PRINCE_PDF_LICENSE_FILE;
+    try{
+        let pdfOptions = {};
         
-    const pdf = await prince(pageContent, pdfOptions);
+        let options = _.defaults({
+            'media'         : 'screen',
+            'page-margin'   : '0.5cm',
+        }, req.query);
+        
+        if(options){
+            _.each(Whitelist_PDFParams, function(param){
+                if(options[param])
+                    pdfOptions[param] = options[param];
+            });
+        }
+        
+        let pageContent = req.body;
+        
+        winston.info(`Render Prince PDF for HTML length ${pageContent.length}`);
 
-    winston.info('Prince PDF generated..');
+        if(config.PRINCE_PDF_LICENSE_FILE)
+            pdfOptions['license-file'] = config.PRINCE_PDF_LICENSE_FILE;
+            
+        const pdf = await prince(pageContent, pdfOptions);
 
-    if ((req.query||{}).attachmentName) {
-        res.attachment(opts.attachmentName);
+        winston.info('Prince PDF generated..');
+
+        if ((req.query||{}).attachmentName) {
+            res.attachment(opts.attachmentName);
+        }
+        res.set(200);
+        res.set('content-type', 'application/pdf');
+        res.send(pdf);
     }
-    res.set(200);
-    res.set('content-type', 'application/pdf');
-    res.send(pdf);
+    catch(err){
+        winston.error(`Error rendering pdf: ${err}`);
+        winston.error(err.stack);
+        res.status(500).send('Error rendering pdf');
+    }
       
 }
 
