35fd87e0b1e4c9aa31f6cb9a0942ec6361e6f67f
Blaise Fonseca <blaisefonseca@hotmail.com>
2018-11-17 13:59:13 +0200

    upload to s3 option

--- src/renderer.js
+++ src/renderer.js
@@ -2,8 +2,19 @@
 const config = require('./config');
 const winston = require('./logger')(__filename);
 const prince    = require('prince-promise');
+const AWS = require('aws-sdk');
 
-const binaryParser = require('superagent-binary-parser');
+let configFile = process.env.CONFIG_FILE;
+let awsConfig = {};
+if(configFile){
+    awsConfig = require(configFile).awsAccessKeys.global;
+}
+let S3 = new AWS.S3({
+    accessKeyId: awsConfig.accessKeyId,
+    secretAccessKey: awsConfig.secretAccessKey,
+    region: 'us-east-1',
+    apiVersion: '2006-03-01'
+});
 
 const Whitelist_PDFParams = [
     'baseurl'     ,
@@ -43,19 +54,51 @@ async function renderPdf(req, res) {
 
         winston.info('Prince PDF generated..');
 
-        if ((req.query||{}).attachmentName) {
-            res.attachment(opts.attachmentName);
+        if(!req.query.link){
+            if ((req.query||{})["attachment-name"]) {
+                res.attachment(req.query["attachment-name"]);
+            }
+            res.status(200);
+            res.set('content-type', 'application/pdf');
+            res.send(pdf);
         }
-        res.set(200);
-        res.set('content-type', 'application/pdf');
-        res.send(pdf);
+        else{
+            let bucket =  (req.query||{}).bucket|| 'pdf-cache-prod/drafts';
+
+            let key = `${((req.query||{})["attachment-name"]||'')}-${guid()}.pdf`
+            let s3Options =  {
+                Bucket      : bucket, 
+                Key         : key,
+                ContentType : 'application/pdf', 
+                Body        : pdf, 
+                ACL         : 'public-read'
+            };
+
+            let s3File = await S3.putObject(s3Options).promise();
+            
+            res.status(200);
+            res.send({url:`https://s3.amazonaws.com/pdf-cache-prod/drafts/${s3Options.Key}`});
+
+        }
+
+        
     }
     catch(err){
         winston.error(`Error rendering pdf: ${err}`);
         winston.error(err.stack);
-        res.status(500).send('Error rendering pdf');
+        res.status(400).send('Error rendering pdf');
     }
       
+}
+function guid(sep) {
+    function s4() {
+        return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
+    }
+
+    if (sep === undefined)
+        sep = '-';
+
+    return s4() + s4() + sep + s4() + sep + s4() + sep + s4() + sep + s4() + s4() + s4();
 }
 
 module.exports = {    
