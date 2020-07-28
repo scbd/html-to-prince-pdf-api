a06085a9e62e1560b6ddfb595c8621226c39f4a8
Stephane <bilodeaustephane@gmail.com>
2020-07-28 15:49:00 -0400

    Enable CORS if CORS_ORGIN is set (for development)

--- src/config.js
+++ src/config.js
@@ -5,6 +5,7 @@ const config = {
   DEBUG_MODE: process.env.DEBUG_MODE === 'true',
   PRINCE_PDF_LICENSE_FILE: process.env.PRINCE_PDF_LICENSE_FILE,
   PRINCE_BINARY : process.env.PRINCE_BINARY,
+  CORS_ORIGIN: process.env.CORS_ORIGIN
 };
 
 module.exports = config;
